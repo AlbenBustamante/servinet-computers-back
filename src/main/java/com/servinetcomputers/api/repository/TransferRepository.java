1bd73aaf04b5834b8f7f28f4689c8bffa96d5851
Alben Bustamante <albenbustamante@gmail.com>
2023-10-29 17:34:00 -0500

    add: new method to get transfers between two dates

--- src/main/java/com/servinetcomputers/api/repository/TransferRepository.java
+++ src/main/java/com/servinetcomputers/api/repository/TransferRepository.java
@@ -3,8 +3,21 @@ package com.servinetcomputers.api.repository;
 import com.servinetcomputers.api.model.Transfer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * The {@link Transfer} repository.
  */
 public interface TransferRepository extends JpaRepository<Transfer, Integer> {
+
+    /**
+     * Search all the transfers created between two dates.
+     *
+     * @param firstDate the first date.
+     * @param lastDate  the second and last date.
+     * @return a list of all the transfers found.
+     */
+    List<Transfer> findAllByCreatedAtBetween(LocalDateTime firstDate, LocalDateTime lastDate);
+
 }
