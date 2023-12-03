c00397b83d356e5154eb9332e887abc24d2ac550
Alben Bustamante <albenbustamante@gmail.com>
2023-12-03 14:32:11 -0500

    feat: enable cors

---
+++ src/main/java/com/servinetcomputers/api/config/CorsConfiguration.java
@@ -0,0 +1,21 @@
+package com.servinetcomputers.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * This is the configuration class to enable CORS.
+ */
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PATCH", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+
+}
