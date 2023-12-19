20092d56cab7b1d4255c40264e88349e22559014
Alben Bustamante <albenbustamante@gmail.com>
2023-12-18 19:44:07 -0500

    feat: upgrade getReports method adding totally in platforms

--- src/main/java/com/servinetcomputers/api/dto/response/DashboardResponse.java
+++ src/main/java/com/servinetcomputers/api/dto/response/DashboardResponse.java
@@ -5,8 +5,14 @@ import java.util.List;
 /**
  * The Admin Dashboard Reports dto model for responses.
  */
-public record DashboardResponse(String total, List<CampusDashboardResponse> campuses) {
+public record DashboardResponse(String total, List<CampusDashboardResponse> campuses,
+                                List<PlatformDetailDashboardResponse> temporaryPlatforms,
+                                List<PlatformDetailDashboardResponse> platforms) {
 
     public record CampusDashboardResponse(int numeral, String total) {
+    }
+
+    public record PlatformDetailDashboardResponse(int platformId, String platformName, int transfersAmount,
+                                                  String total) {
     }
 }
--- src/main/java/com/servinetcomputers/api/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/servinetcomputers/api/service/impl/DashboardServiceImpl.java
@@ -2,6 +2,7 @@ package com.servinetcomputers.api.service.impl;
 
 import com.servinetcomputers.api.dto.response.DashboardResponse;
 import com.servinetcomputers.api.dto.response.DashboardResponse.CampusDashboardResponse;
+import com.servinetcomputers.api.dto.response.DashboardResponse.PlatformDetailDashboardResponse;
 import com.servinetcomputers.api.exception.UserNotFoundException;
 import com.servinetcomputers.api.exception.UserUnavailableException;
 import com.servinetcomputers.api.model.User;
@@ -36,13 +37,16 @@ public class DashboardServiceImpl implements IDashboardService {
     public DashboardResponse getReports(int userId) {
         final var user = getUser(userId);
 
-        final var startDate = LocalDateTime.of(LocalDate.now(DEFAULT_ZONE), LocalTime.MIN);
-        final var endDate = LocalDateTime.of(LocalDate.now(DEFAULT_ZONE), LocalTime.now(DEFAULT_ZONE));
+        final var today = LocalDate.now(DEFAULT_ZONE);
+        final var startDate = LocalDateTime.of(today, LocalTime.MIN);
+        final var endDate = LocalDateTime.of(today, LocalTime.now(DEFAULT_ZONE));
 
         final List<CampusDashboardResponse> campuses = new ArrayList<>(user.getCampuses().size());
+        final List<PlatformDetailDashboardResponse> temporaryPlatforms = new ArrayList<>();
         var total = BigDecimal.ZERO;
 
         for (final var campus : user.getCampuses()) {
+            /* -- CAMPUSES -- */
             var campusTotal = BigDecimal.ZERO;
 
             final var transfers = transferRepository
@@ -55,9 +59,48 @@ public class DashboardServiceImpl implements IDashboardService {
             campuses.add(new CampusDashboardResponse(campus.getNumeral(), currencyFormatter.format(campusTotal)));
 
             total = total.add(campusTotal);
+
+            /* -- PLATFORMS -- */
+            for (final var platform : campus.getPlatforms()) {
+                var platformTotal = BigDecimal.ZERO;
+                var transfersAmount = 0;
+
+                for (final var transfer : platform.getTransfers()) {
+                    if (transfer.getCreatedAt().toLocalDate().equals(today) && transfer.getCampusId().equals(campus.getId())) {
+                        platformTotal = platformTotal.add(transfer.getValue());
+                        transfersAmount++;
+                    }
+                }
+
+                temporaryPlatforms.add(new PlatformDetailDashboardResponse(
+                        platform.getId(), platform.getName(), transfersAmount, String.valueOf(platformTotal))
+                );
+            }
         }
 
-        return new DashboardResponse(currencyFormatter.format(total), campuses);
+        final List<PlatformDetailDashboardResponse> platforms = new ArrayList<>();
+
+        temporaryPlatforms.forEach(platform -> {
+            var platformValue = BigDecimal.ZERO;
+            var transfersAmount = 0;
+
+            for (final var childPlatform : temporaryPlatforms) {
+                if (platform.platformId() == childPlatform.platformId()) {
+                    platformValue = platformValue.add(new BigDecimal(childPlatform.total()));
+                    transfersAmount += childPlatform.transfersAmount();
+                }
+            }
+
+            final var alreadyExists = platforms.stream().anyMatch(p -> p.platformId() == platform.platformId());
+
+            if (!alreadyExists) {
+                platforms.add(new PlatformDetailDashboardResponse(
+                        platform.platformId(), platform.platformName(), transfersAmount, currencyFormatter.format(platformValue)
+                ));
+            }
+        });
+
+        return new DashboardResponse(currencyFormatter.format(total), campuses, temporaryPlatforms, platforms);
     }
 
     private User getUser(int userId) {
