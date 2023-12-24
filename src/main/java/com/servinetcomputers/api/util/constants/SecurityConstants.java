a0ee4bb7aace2a13439ebd5d0ffaa047619f2350
Alben Bustamante <albenbustamante@gmail.com>
2023-12-24 18:22:52 -0500

    fix: white list typo

--- src/main/java/com/servinetcomputers/api/util/constants/SecurityConstants.java
+++ src/main/java/com/servinetcomputers/api/util/constants/SecurityConstants.java
@@ -4,7 +4,7 @@ package com.servinetcomputers.api.util.constants;
  * The security constants.
  */
 public final class SecurityConstants {
-    public static final String[] WHITE_LIST = {"/auth", "/auth/**", "/swagger-ui.html", "/swagger-ui", "/api-docs", "/swagger-ui/**", "/v3/ai-docs/**" };
+    public static final String[] WHITE_LIST = {"/auth", "/auth/**", "/swagger-ui.html", "/swagger-ui", "/api-docs", "/swagger-ui/**", "/v3/api-docs/**"};
 
     private SecurityConstants() {
     }
