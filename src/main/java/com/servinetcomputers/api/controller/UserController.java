0e856c04129d9ff3315893c355993c666a000c74
Alben Bustamante <albenbustamante@gmail.com>
2023-12-24 13:41:39 -0500

    feat: new endpoints to authenticacion

--- src/main/java/com/servinetcomputers/api/controller/UserController.java
+++ src/main/java/com/servinetcomputers/api/controller/UserController.java
@@ -9,13 +9,11 @@ import com.servinetcomputers.api.service.ICampusService;
 import com.servinetcomputers.api.service.IDashboardService;
 import com.servinetcomputers.api.service.IUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,11 +29,6 @@ public class UserController {
     private final IUserService userService;
     private final ICampusService campusService;
     private final IDashboardService dashboardService;
-
-    @PostMapping
-    public ResponseEntity<PageResponse<UserResponse>> register(@RequestBody UserRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(request));
-    }
 
     @PatchMapping("/{userId}")
     public ResponseEntity<PageResponse<UserResponse>> update(@PathVariable("userId") int userId, @RequestBody UserRequest request) {
