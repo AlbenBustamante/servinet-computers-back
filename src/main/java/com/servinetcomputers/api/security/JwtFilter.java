586c86f077cf926bff9ac6bf8665a5d799d433af
Alben Bustamante <albenbustamante@gmail.com>
2023-12-24 12:21:24 -0500

    create: jwt filter

---
+++ src/main/java/com/servinetcomputers/api/security/JwtFilter.java
@@ -0,0 +1,43 @@
+package com.servinetcomputers.api.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * The JWT Authentication Filter.
+ */
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final var header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        try {
+            final var auth = jwtProvider.validateToken(header.replace("Bearer ", ""));
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        } catch (Exception ex) {
+            SecurityContextHolder.clearContext();
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
