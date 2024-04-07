27a0c46211b060af44c05e7691f4b225fdafb5b8
Alben Bustamante <albenbustamante@gmail.com>
2024-04-07 16:03:31 -0500

    create: cash register detail repository

---
+++ src/main/java/com/servinetcomputers/api/domain/cashregister/abs/CashRegisterDetailRepository.java
@@ -0,0 +1,13 @@
+package com.servinetcomputers.api.domain.cashregister.abs;
+
+import com.servinetcomputers.api.domain.cashregister.entity.CashRegisterDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public interface CashRegisterDetailRepository extends JpaRepository<CashRegisterDetail, Integer> {
+
+    Optional<CashRegisterDetail> findByCreatedByAndCreatedDateBetween(String createdBy, LocalDateTime firstDate, LocalDateTime lastDate);
+
+}
