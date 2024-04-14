86081c0ce221a229e689f9b1fd1724b30e4fc631
Alben Bustamante <albenbustamante@gmail.com>
2024-04-14 09:35:53 -0500

    define: cash register balance entity

---
+++ src/main/java/com/servinetcomputers/api/domain/cashregister/util/CashRegisterBalanceConstants.java
@@ -0,0 +1,8 @@
+package com.servinetcomputers.api.domain.cashregister.util;
+
+public final class CashRegisterBalanceConstants {
+    public static final int BASE_LENGTH = 65;
+
+    private CashRegisterBalanceConstants() {
+    }
+}
