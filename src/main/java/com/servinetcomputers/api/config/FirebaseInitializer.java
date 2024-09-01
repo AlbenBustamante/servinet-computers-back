b7f12f769b3b3beebce534de82d6b6f61f28ac53
Alben Bustamante <albenbustamante@gmail.com>
2024-09-01 18:44:39 -0500

    update: firebase credentials

--- src/main/java/com/servinetcomputers/api/config/FirebaseInitializer.java
+++ src/main/java/com/servinetcomputers/api/config/FirebaseInitializer.java
@@ -5,31 +5,27 @@ import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.servinetcomputers.api.exception.AppException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseInitializer {
+
+    @Value("${service-account-key-path}")
+    private String serviceAccountKeyPath;
 
     @Value("${bucket-name}")
     private String bucketName;
 
     @PostConstruct
     private void initialize() throws IOException {
-        final var serviceAccountKey = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
-
-        if (serviceAccountKey == null) {
-            throw new AppException("serviceAccountKey is null", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
         final var options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccountKey))
+                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(serviceAccountKeyPath)))
                 .setStorageBucket(bucketName)
                 .build();
 
@@ -40,14 +36,8 @@ public class FirebaseInitializer {
 
     @Bean
     public Storage storage() throws IOException {
-        final var serviceAccountKey = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
-
-        if (serviceAccountKey == null) {
-            throw new AppException("serviceAccountKey is null", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
         return StorageOptions.newBuilder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccountKey))
+                .setCredentials(GoogleCredentials.fromStream(new FileInputStream(serviceAccountKeyPath)))
                 .build()
                 .getService();
     }
