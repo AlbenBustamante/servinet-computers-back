3d6567cecfb0031503141a66dfa2b9356da4b647
Alben Bustamante <albenbustamante@gmail.com>
2024-09-06 00:03:13 -0500

    feat: new dashboard endpoint

--- src/main/java/com/servinetcomputers/api/domain/user/UserController.java
+++ src/main/java/com/servinetcomputers/api/domain/user/UserController.java
@@ -1,19 +1,11 @@
 package com.servinetcomputers.api.domain.user;
 
-import com.servinetcomputers.api.domain.dashboard.abs.IDashboardService;
-import com.servinetcomputers.api.domain.dashboard.dto.DashboardResponse;
 import com.servinetcomputers.api.domain.user.abs.IUserService;
 import com.servinetcomputers.api.domain.user.dto.UserRequest;
 import com.servinetcomputers.api.domain.user.dto.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +18,6 @@ import java.util.List;
 public class UserController {
 
     private final IUserService userService;
-    private final IDashboardService dashboardService;
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> getAll() {
@@ -46,11 +37,6 @@ public class UserController {
     @DeleteMapping("/{userId}")
     public ResponseEntity<Boolean> delete(@PathVariable("userId") int userId) {
         return ResponseEntity.ok(userService.delete(userId));
-    }
-
-    @GetMapping("/{userId}/reports")
-    public ResponseEntity<DashboardResponse> getReports(@PathVariable("userId") int userId) {
-        return ResponseEntity.ok(dashboardService.getReports(userId));
     }
 
 }
