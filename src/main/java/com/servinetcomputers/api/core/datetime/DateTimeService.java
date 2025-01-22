8ce26c3f2896b95569c0d4a9d67d98e4b856c31e
Alben Bustamante <albenbustamante@gmail.com>
2025-01-22 11:47:32 -0500

    feat: new date time service

---
+++ src/main/java/com/servinetcomputers/api/core/datetime/DateTimeService.java
@@ -0,0 +1,15 @@
+package com.servinetcomputers.api.core.datetime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public interface DateTimeService {
+    LocalDate dateNow();
+
+    LocalTime timeNow();
+
+    LocalDateTime getMinByDate(LocalDate date);
+
+    LocalDateTime getMaxByDate(LocalDate date);
+}
---
+++ src/main/java/com/servinetcomputers/api/core/datetime/DateTimeServiceImpl.java
@@ -0,0 +1,35 @@
+package com.servinetcomputers.api.core.datetime;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+
+@RequiredArgsConstructor
+@Component
+public class DateTimeServiceImpl implements DateTimeService {
+    private final ZoneId zoneId;
+
+    @Override
+    public LocalDate dateNow() {
+        return LocalDate.now(zoneId);
+    }
+
+    @Override
+    public LocalTime timeNow() {
+        return LocalTime.now(zoneId);
+    }
+
+    @Override
+    public LocalDateTime getMinByDate(LocalDate date) {
+        return LocalDateTime.of(date, LocalTime.MIN);
+    }
+
+    @Override
+    public LocalDateTime getMaxByDate(LocalDate date) {
+        return LocalDateTime.of(date, LocalTime.now(zoneId));
+    }
+}
