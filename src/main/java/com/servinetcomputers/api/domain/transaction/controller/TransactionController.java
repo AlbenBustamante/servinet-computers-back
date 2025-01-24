8bb50d0678aa91797174c83b6e0166831b067090
Alben Bustamante <albenbustamante@gmail.com>
2025-01-24 16:41:46 -0500

    refactor: transactions

--- src/main/java/com/servinetcomputers/api/domain/transaction/controller/TransactionController.java
+++ src/main/java/com/servinetcomputers/api/domain/transaction/controller/TransactionController.java
@@ -1,10 +1,10 @@
 package com.servinetcomputers.api.domain.transaction.controller;
 
+import com.servinetcomputers.api.domain.transaction.application.usecase.CreateTransactionDetailUseCase;
 import com.servinetcomputers.api.domain.transaction.application.usecase.GetAllTransactionsUseCase;
 import com.servinetcomputers.api.domain.transaction.domain.dto.TransactionDetailRequest;
 import com.servinetcomputers.api.domain.transaction.domain.dto.TransactionDetailResponse;
 import com.servinetcomputers.api.domain.transaction.domain.dto.TransactionResponse;
-import com.servinetcomputers.api.domain.transaction.domain.repository.TransactionDetailRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +16,11 @@ import java.util.List;
 @RestController
 public class TransactionController {
     private final GetAllTransactionsUseCase getAllTransactionsUseCase;
-    private final TransactionDetailRepository detailService;
+    private final CreateTransactionDetailUseCase createTransactionDetailUseCase;
 
     @PostMapping
     public ResponseEntity<TransactionDetailResponse> register(@RequestBody TransactionDetailRequest request) {
-        return ResponseEntity.ok(detailService.create(request));
+        return ResponseEntity.ok(createTransactionDetailUseCase.call(request));
     }
 
     @GetMapping
