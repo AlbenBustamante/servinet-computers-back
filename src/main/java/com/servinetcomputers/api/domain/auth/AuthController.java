a66f643c9ab3f9e1cf7db56d6be3f13cb4c1e128
Alben Bustamante <albenbustamante@gmail.com>
2025-01-24 18:17:25 -0500

    update: auth controller

--- src/main/java/com/servinetcomputers/api/domain/auth/AuthController.java
+++ src/main/java/com/servinetcomputers/api/domain/auth/AuthController.java
@@ -3,9 +3,9 @@ package com.servinetcomputers.api.domain.auth;
 import com.servinetcomputers.api.domain.auth.abs.IAuthService;
 import com.servinetcomputers.api.domain.auth.dto.AuthRequest;
 import com.servinetcomputers.api.domain.auth.dto.AuthResponse;
+import com.servinetcomputers.api.domain.user.application.usecase.CreateUserUseCase;
 import com.servinetcomputers.api.domain.user.domain.dto.UserRequest;
 import com.servinetcomputers.api.domain.user.domain.dto.UserResponse;
-import com.servinetcomputers.api.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -16,28 +16,26 @@ import org.springframework.web.bind.annotation.*;
  * The authentication routes/endpoints.
  */
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping(path = "/auth")
 @RestController
 public class AuthController {
-
-    private final UserRepository userService;
+    private final CreateUserUseCase createUserUseCase;
     private final IAuthService authService;
 
-    @PostMapping("/register")
+    @PostMapping(path = "/register")
     public ResponseEntity<UserResponse> register(@RequestBody UserRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(createUserUseCase.call(request));
     }
 
-    @PostMapping("/sign-in")
+    @PostMapping(path = "/sign-in")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
 
-    @PostMapping("/sign-out")
+    @PostMapping(path = "/sign-out")
     public ResponseEntity<Boolean> logout(@RequestHeader(name = HttpHeaders.AUTHORIZATION) String token) {
         return ResponseEntity.ok(authService.logout(token));
     }
-
 }
 
 
