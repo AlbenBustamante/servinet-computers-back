65de100ff34249dda23f1ce6c838becbffcb85a2
Alben Bustamante <albenbustamante@gmail.com>
2025-02-04 18:37:32 -0500

    add: new model

---
+++ src/main/java/com/servinetcomputers/api/module/cashregister/domain/dto/AdmCashRegistersDto.java
@@ -0,0 +1,9 @@
+package com.servinetcomputers.api.module.cashregister.domain.dto;
+
+import java.util.List;
+
+public record AdmCashRegistersDto(
+        List<CashRegisterDetailResponse> currentCashRegisters,
+        List<CashRegisterDetailResponse> pendingCashRegisters
+) {
+}
