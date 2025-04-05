b04730ee2cd8e59c9d7077919cbb4fe5a1aa8a4e
Alben Bustamante <albenbustamante@gmail.com>
2025-04-05 18:17:51 -0500

    feat: enable endpoint for expense updating

--- src/main/java/com/servinetcomputers/api/module/expense/controller/ExpenseController.java
+++ src/main/java/com/servinetcomputers/api/module/expense/controller/ExpenseController.java
@@ -2,8 +2,10 @@ package com.servinetcomputers.api.module.expense.controller;
 
 import com.servinetcomputers.api.module.expense.application.usecase.CreateExpenseUseCase;
 import com.servinetcomputers.api.module.expense.application.usecase.DeleteExpenseUseCase;
+import com.servinetcomputers.api.module.expense.application.usecase.UpdateExpenseUseCase;
 import com.servinetcomputers.api.module.expense.domain.dto.ExpenseRequest;
 import com.servinetcomputers.api.module.expense.domain.dto.ExpenseResponse;
+import com.servinetcomputers.api.module.expense.domain.dto.UpdateExpenseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +15,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ExpenseController {
     private final CreateExpenseUseCase createExpenseUseCase;
+    private final UpdateExpenseUseCase updateExpenseUseCase;
     private final DeleteExpenseUseCase deleteExpenseUseCase;
 
     @PostMapping
     public ResponseEntity<ExpenseResponse> register(@RequestBody ExpenseRequest request) {
         return ResponseEntity.ok(createExpenseUseCase.call(request));
+    }
+
+    @PatchMapping(path = "/{id}")
+    public ResponseEntity<ExpenseResponse> update(@PathVariable("id") int expenseId, @RequestBody UpdateExpenseDto dto) {
+        return ResponseEntity.ok(updateExpenseUseCase.call(expenseId, dto));
     }
 
     @DeleteMapping(path = "/{id}")
