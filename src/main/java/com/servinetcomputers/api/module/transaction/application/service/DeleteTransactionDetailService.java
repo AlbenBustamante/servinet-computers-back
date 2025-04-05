38fda0ff25fa08cc065b9ae2e41bc1eaeb0e138b
Alben Bustamante <albenbustamante@gmail.com>
2025-04-05 17:13:26 -0500

    update: delete transaction detail service for change log registry

--- src/main/java/com/servinetcomputers/api/module/transaction/application/service/DeleteTransactionDetailService.java
+++ src/main/java/com/servinetcomputers/api/module/transaction/application/service/DeleteTransactionDetailService.java
@@ -4,8 +4,13 @@ import com.servinetcomputers.api.core.exception.AppException;
 import com.servinetcomputers.api.core.exception.InvalidTempCodeException;
 import com.servinetcomputers.api.core.exception.NotFoundException;
 import com.servinetcomputers.api.core.exception.RequiredTempCodeException;
+import com.servinetcomputers.api.core.util.enums.ChangeLogAction;
+import com.servinetcomputers.api.core.util.enums.ChangeLogType;
+import com.servinetcomputers.api.module.changelog.application.usecase.CreateChangeLogUseCase;
+import com.servinetcomputers.api.module.changelog.domain.dto.CreateChangeLogDto;
 import com.servinetcomputers.api.module.tempcode.domain.repository.TempCodeRepository;
 import com.servinetcomputers.api.module.transaction.application.usecase.DeleteTransactionDetailUseCase;
+import com.servinetcomputers.api.module.transaction.domain.dto.TransactionDetailResponse;
 import com.servinetcomputers.api.module.transaction.domain.repository.TransactionDetailRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class DeleteTransactionDetailService implements DeleteTransactionDetailUseCase {
     private final TransactionDetailRepository transactionDetailRepository;
     private final TempCodeRepository tempCodeRepository;
+    private final CreateChangeLogUseCase createChangeLogUseCase;
 
     @Transactional(rollbackFor = AppException.class)
     @Override
@@ -33,12 +39,29 @@ public class DeleteTransactionDetailService implements DeleteTransactionDetailUs
         final var transactionDetail = transactionDetailRepository.get(transactionDetailId)
                 .orElseThrow(() -> new NotFoundException("No se encontró la jornada #" + transactionDetailId));
 
+        final var previousData = TransactionDetailResponse.copyWith(transactionDetail);
+
         transactionDetail.setEnabled(false);
         transactionDetailRepository.save(transactionDetail);
+
+        createChangeLog(previousData);
 
         final var user = transactionDetail.getCashRegisterDetail().getUser();
 
         lastTempCode.get().setUsedBy(user);
         tempCodeRepository.save(lastTempCode.get());
+    }
+
+    private void createChangeLog(TransactionDetailResponse previousData) {
+        final var dto = new CreateChangeLogDto(
+                ChangeLogAction.DELETE,
+                ChangeLogType.TRANSACTION_DETAIL,
+                previousData.getCashRegisterDetailId(),
+                previousData.getCashRegisterDetail().getStatus(),
+                previousData,
+                null
+        );
+
+        createChangeLogUseCase.call(dto);
     }
 }
