dccb2a54b8b28b7b8ecf9e100e11c2bc639b9574
Alben Bustamante <albenbustamante@gmail.com>
2025-04-07 08:21:32 -0500

    fix: objectmapper bug with parameter names

--- src/main/java/com/servinetcomputers/api/core/config/JsonConfig.java
+++ src/main/java/com/servinetcomputers/api/core/config/JsonConfig.java
@@ -3,6 +3,7 @@ package com.servinetcomputers.api.core.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +12,8 @@ public class JsonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         final var objectMapper = new ObjectMapper();
-        
+
+        objectMapper.registerModule(new ParameterNamesModule());
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
