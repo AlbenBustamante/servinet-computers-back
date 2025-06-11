0cd3619aad630bb42b4dc0855a0f2fda92b0b515
Alben Bustamante <albenbustamante@gmail.com>
2025-06-11 13:34:50 -0500

    add: total of transactions in journeys

--- src/main/java/com/servinetcomputers/api/module/user/domain/dto/JourneyDetailDto.java
+++ src/main/java/com/servinetcomputers/api/module/user/domain/dto/JourneyDetailDto.java
@@ -3,14 +3,16 @@ package com.servinetcomputers.api.module.user.domain.dto;
 import java.util.List;
 
 /**
- * @param totalOfHours     Cantidad total de horas trabajadas en el mes.
- * @param totalOfDiscounts Cantidad total de dinero a descontar del mes.
- * @param journeys         Jornadas trabajadas en el mes.
+ * @param totalOfHours        Cantidad total de horas trabajadas en el mes.
+ * @param totalOfDiscounts    Cantidad total de dinero a descontar del mes.
+ * @param totalOfTransactions Cantidad total de transacciones del mes.
+ * @param journeys            Jornadas trabajadas en el mes.
  * @see JourneyDto
  */
 public record JourneyDetailDto(
         String totalOfHours,
         Integer totalOfDiscounts,
+        Integer totalOfTransactions,
         List<JourneyDto> journeys
 ) {
 }
