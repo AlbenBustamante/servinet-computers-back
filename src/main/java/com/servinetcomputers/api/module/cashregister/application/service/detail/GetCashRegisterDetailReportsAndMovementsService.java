7b723159138c9b47e678f6ea34645710687f76cf
Alben Bustamante <albenbustamante@gmail.com>
2025-06-29 10:08:02 -0500

    add: service call to set cash transfer details

--- src/main/java/com/servinetcomputers/api/module/cashregister/application/service/detail/GetCashRegisterDetailReportsAndMovementsService.java
+++ src/main/java/com/servinetcomputers/api/module/cashregister/application/service/detail/GetCashRegisterDetailReportsAndMovementsService.java
@@ -1,16 +1,16 @@
 package com.servinetcomputers.api.module.cashregister.application.service.detail;
 
 import com.servinetcomputers.api.core.exception.NotFoundException;
-import com.servinetcomputers.api.core.util.enums.CashBoxType;
 import com.servinetcomputers.api.module.cashregister.application.usecase.detail.GetCashRegisterDetailReportsAndMovementsUseCase;
+import com.servinetcomputers.api.module.cashregister.application.usecase.detail.GetCashTransfersByIdUseCase;
 import com.servinetcomputers.api.module.cashregister.domain.dto.CashRegisterDetailMovementsDto;
 import com.servinetcomputers.api.module.cashregister.domain.dto.DetailedCashRegisterTransactionsDto;
 import com.servinetcomputers.api.module.cashregister.domain.repository.CashRegisterDetailRepository;
-import com.servinetcomputers.api.module.cashtransfer.domain.repository.CashTransferRepository;
 import com.servinetcomputers.api.module.changelog.domain.repository.ChangeLogRepository;
 import com.servinetcomputers.api.module.expense.domain.repository.ExpenseRepository;
 import com.servinetcomputers.api.module.transaction.domain.repository.TransactionDetailRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,9 +23,9 @@ public class GetCashRegisterDetailReportsAndMovementsService implements GetCashR
     private final CashRegisterDetailRepository repository;
     private final TransactionDetailRepository transactionDetailRepository;
     private final ExpenseRepository expenseRepository;
-    private final CashTransferRepository cashTransferRepository;
     private final ChangeLogRepository changeLogRepository;
     private final com.servinetcomputers.api.module.reports.application.usecase.GetCashRegisterDetailReportsUseCase getCashRegisterDetailReportsUseCase;
+    private final GetCashTransfersByIdUseCase getCashTransfersByIdUseCase;
 
     /**
      * Get all detailed movements made by a cash register detail by its ID.
@@ -44,9 +44,9 @@ public class GetCashRegisterDetailReportsAndMovementsService implements GetCashR
         final var transactions = transactionDetailRepository.getAllByCashRegisterDetailId(cashRegisterDetailId);
         final var expenses = expenseRepository.getAllByCashRegisterDetailIdAndDiscount(cashRegisterDetailId, false);
         final var discounts = expenseRepository.getAllByCashRegisterDetailIdAndDiscount(cashRegisterDetailId, true);
-        final var transfers = cashTransferRepository.getAllByCashBoxIdAndType(cashRegisterDetailId, CashBoxType.CASH_REGISTER);
+        final var transfers = getCashTransfersByIdUseCase.call(cashRegisterDetailId, Pageable.unpaged());
 
-        final var detailedTransactions = new DetailedCashRegisterTransactionsDto(transactions, expenses, discounts, transfers);
+        final var detailedTransactions = new DetailedCashRegisterTransactionsDto(transactions, expenses, discounts, transfers.getContent());
 
         final var changeLogs = changeLogRepository.getAllByCashRegisterDetailId(cashRegisterDetailId);
 
